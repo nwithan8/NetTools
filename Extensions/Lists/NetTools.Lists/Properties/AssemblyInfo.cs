91f1898b131530ffdeaf0274e842cc02078a03a3
nharris <nharris@easypost.com>
2022-10-14 18:26:36 -0600

    - Add version details - Change NuGet spec file

---
+++ Extensions/Lists/NetTools.Lists/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NetTools.Lists")]
+[assembly: AssemblyDescription("NetTools.Lists is a NetTools extension for lists and sorting")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("NetTools.Lists")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
